9859c1ac269c3e652de0957f154b06f621a27532
yash bhatt <bhatty048@gmail.com>
2025-07-08 15:32:41 +0530

    learnt and implemented  state and re rendered data

--- Chef Claude/src/components/Content.jsx
+++ Chef Claude/src/components/Content.jsx
@@ -1,8 +1,31 @@
-export default function Content(){
-    return(
-        <div className="main">
-            <input type="text" placeholder="e.g. oregano"/>
-            <button type="button">+Add ingredient</button>
+import React from "react";
+
+export default function Content() {
+    const [currArr, setItems] = React.useState([]);
+
+const ingredientsListItems = currArr.map((i, idx) => (
+    <li key={`${i}-${idx}`}>{i}</li>
+));
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        const formData = new FormData(event.currentTarget);
+        const newIngredient = formData.get("ingredient");
+        setItems(prevArr => [...prevArr, newIngredient]);
+    }
+    return (
+        <div className="main">  <form onSubmit={handleSubmit} className="add-ingredient-form">
+                <input
+                    type="text"
+                    placeholder="e.g. oregano"
+                    aria-label="Add ingredient"
+                    name="ingredient"
+                />
+                <button>Add ingredient</button>
+            </form>
+            <ul>
+                {ingredientsListItems}
+            </ul>
         </div>
-    )
+    );
 }
